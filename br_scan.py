b027ed107d2d77b2cd296b88d207d1d59063bf39
Baraas002 <be.sahmoud@std.alaqsa.edu.ps>
2023-06-05 19:16:57 +0300

    br_scan

--- br_scan.py
+++ br_scan.py
@@ -1,56 +1,53 @@
 import socket
-import re # use to validate IP address format and get the ip of this domain
+import re  
+import requests 
 
-# Function to scan ports
+
+
+
 def scan_ports(target_host, start_port, end_port):
     open_ports = []
     for port in range(start_port, end_port + 1):
         # Create a socket object
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)  # Set a timeout value for the connection
-
-        # Attempt to connect to the port
+        sock.settimeout(1)  
         result = sock.connect_ex((target_host, port))
         if result == 0:
             open_ports.append(port)
-
         sock.close()
-
     return open_ports
 
-# Function to validate IP address format
+
 def validate_ip(ip):
     pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
-    return pattern.match(ip)
+    return pattern.match(ip)  
+
 
 if __name__ == '__main__':
-
-
-
-
+    
+    
+    
+    #######################################################################################
     from termcolor import colored
     from pyfiglet import Figlet
 
     fig = Figlet(font='slant')
-
     print(colored(fig.renderText('Baraa Scan'), 'green'))
-
-    from termcolor import colored
     print(colored('Create by Eng Baraa Sahmoud', 'red', attrs=['reverse', 'blink']))
-
-
+    #######################################################################################
     while True:
         print("\nMenu:")
         print("*** Baraa Scan ***")
-        print("1. Get localhost")  # Get the local host name
-        print("2. Get the service name for this port")# Get the service name for a port
+        print("1. Get localhost")
+        print("2. Get the service name for this port")
         print("3. Port scanning")
         print("4. Check if a port is open")
-        print("5. Get the ip of this domain")
-        print("6. Exit")
+        print("5. Get the IP of this domain")
+        print("6. DNS lookup")  
+        print("7. IP Locator") 
+        print("8. Exit")
         print("*** ☠️ ***")
-
-        choice = input("Enter your choice (1-5): ")
+        choice = input("Enter your choice (1-8): ")
 
         if choice == "1":
             hostname = socket.gethostname()
@@ -64,12 +61,8 @@ if __name__ == '__main__':
             except OSError:
                 print("Error retrieving service name")
 
-
-
-
         elif choice == "3":
             target_host = input("Enter the target IP address or hostname: ")
-
             # Validate IP address
             if not validate_ip(target_host):
                 print("Invalid target host IP address")
@@ -89,7 +82,7 @@ if __name__ == '__main__':
         elif choice == "4":
             target_host = input("Enter the target IP address or hostname: ")
 
-            # Validate IP address
+            
             if not validate_ip(target_host):
                 print("Invalid target host IP address")
                 continue
@@ -97,8 +90,7 @@ if __name__ == '__main__':
             port = int(input("Enter the port number: "))
 
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(1)  # Set a timeout value for the connection
-
+            sock.settimeout(1)
             result = sock.connect_ex((target_host, port))
             if result == 0:
                 print(f"Port {port} is open")
@@ -106,17 +98,47 @@ if __name__ == '__main__':
                 print(f"Port {port} is closed")
 
             sock.close()
+
         elif choice == "5":
             domain = input("Enter the domain name: ")
             try:
                 ip_address = socket.gethostbyname(domain)
                 print(f"IP address for domain {domain}: {ip_address}")
-            except socket.gaierror:
+            except socket.gaierror:# gaierror: get address info error
                 print("Failed to get IP address for the domain")
 
         elif choice == "6":
+            domain = input("Enter the domain name: ")
+            try:
+                ip_addresses = socket.getaddrinfo(domain, None)
+                print(f"IP addresses for domain {domain}:")
+                for ip in ip_addresses:
+                    print(ip[4][0])
+            except socket.gaierror:
+                print("Failed to get IP addresses for the domain")
+
+        elif choice == "7":
+            ip_address = input("Enter the IP address to locate: ")
+            url = f"http://ip-api.com/json/{ip_address}"
+            response = requests.get(url)
+            data = response.json()
+            if data["status"] == "fail":
+                print("Failed to locate the IP address")
+            else:
+                print("IP Locator Results:")
+                print(f"IP Address: {data['query']}")
+                print(f"Country: {data['country']}")
+                print(f"Region: {data['regionName']}")
+                print(f"City: {data['city']}")
+                print(f"ZIP Code: {data['zip']}")
+                print(f"Latitude: {data['lat']}")
+                print(f"Longitude: {data['lon']}")
+                
+        elif choice == "8":
             print("Exiting...")
             break
+
+
 
         else:
             print("Invalid choice. Please try again.")
